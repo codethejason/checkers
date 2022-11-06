0d43b0992bfda5eecf6048f815f0ba538689df2e
Android-JCPP-Servo <astewart1138@gmail.com>
2022-11-05 21:03:01 -0500

    Set tiles to switch colors for board reorientation

--- board_orientation.js
+++ board_orientation.js
@@ -2,37 +2,30 @@ window.addEventListener('load', () => {
     console.log('page is fully loaded');
 
     // INITIALIZE GAMEBOARD
-    let gameBoard = [];
-
+    let gameBoard = document.getElementById('board');
+    let gameBoardTiles = document.querySelectorAll('#board .tile');
+    console.log('Game Board', gameBoardTiles);
+    
     // GET DIRECTIONAL OPTIONS
     let normal = document.getElementById('default');
     let reversed = document.getElementById('reversed');
 
+    // TEST! TEST! TEST!
+    console.log('Normal', gameBoardTiles.length);
+    
     // LISTEN FOR COLOR BUTTON CLICKS
     // NORMAL
     normal.addEventListener('click', () => {
-        gameBoard = [
-            [0, 1, 0, 1, 0, 1, 0, 1],
-            [1, 0, 1, 0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0, 1, 0, 1],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [2, 0, 2, 0, 2, 0, 2, 0],
-            [0, 2, 0, 2, 0, 2, 0, 2],
-            [2, 0, 2, 0, 2, 0, 2, 0]
-        ];
+        gameBoardTiles.forEach(tile => {
+            tile.style.backgroundColor = '#232621';
+        });
+        gameBoard.style.backgroundColor = '#B93848';
     });
     // REVERSED
     reversed.addEventListener('click', () => {
-        gameBoard = [
-            [1, 0, 1, 0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0, 1, 0, 1],
-            [1, 0, 1, 0, 1, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 2, 0, 2, 0, 2, 0, 2],
-            [2, 0, 2, 0, 2, 0, 2, 0],
-            [0, 2, 0, 2, 0, 2, 0, 2]
-        ];
+        gameBoardTiles.forEach(tile => {
+            tile.style.backgroundColor = '#B93848';
+        });
+        gameBoard.style.backgroundColor = '#232621';
     });
 })
