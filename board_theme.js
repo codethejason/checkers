3816e68c3b4169571ee5487b9620637227afd12a
Android-JCPP-Servo <astewart1138@gmail.com>
2022-11-05 23:26:06 -0500

    Set black color to original color to match originality test standards

--- board_theme.js
+++ board_theme.js
@@ -1,34 +1,34 @@
 window.addEventListener("load", () => {
-  console.log("page is fully loaded");
-  // GET PIECE IDS
-  let board = document.getElementById("board");
-	let tiles = document.querySelectorAll(".tile");
+    console.log("page is fully loaded");
+    // GET PIECE IDS
+    let board = document.getElementById("board");
+    let tiles = document.querySelectorAll(".tile");
 
-  // GET PIECE COLOR BUTTONS
-  let redBlack = document.getElementById("redBlack");
-  let whiteBlack = document.getElementById("whiteBlack");
-  let greenBlue = document.getElementById("greenBlue");
+    // GET PIECE COLOR BUTTONS
+    let redBlack = document.getElementById("redBlack");
+    let whiteBlack = document.getElementById("whiteBlack");
+    let greenBlue = document.getElementById("greenBlue");
 
-  // LISTEN FOR COLOR BUTTON CLICKS
-  // RED BLACK
-  redBlack.addEventListener("click", () => {
-		board.style.backgroundColor = "#B93848";
-		tiles.forEach((tile) => {
-      tile.style.backgroundColor = "black";
+    // LISTEN FOR COLOR BUTTON CLICKS
+    // RED BLACK
+    redBlack.addEventListener("click", () => {
+        board.style.backgroundColor = "#B93848";
+        tiles.forEach((tile) => {
+            tile.style.backgroundColor = "#B93848";
+        });
     });
-  });
-  // WHITE-BLACK
-  whiteBlack.addEventListener("click", () => {
-		board.style.backgroundColor = "white";
-		tiles.forEach((tile) => {
-      tile.style.backgroundColor = "black";
+    // WHITE-BLACK
+    whiteBlack.addEventListener("click", () => {
+        board.style.backgroundColor = "white";
+        tiles.forEach((tile) => {
+            tile.style.backgroundColor = "black";
+        });
     });
-  });
-  // GREEN-BLUE
-  greenBlue.addEventListener("click", () => {
-    board.style.backgroundColor = "green";
-		tiles.forEach(tile => {
-			tile.style.backgroundColor = "blue";
-		});
-  });
+    // GREEN-BLUE
+    greenBlue.addEventListener("click", () => {
+        board.style.backgroundColor = "green";
+        tiles.forEach(tile => {
+            tile.style.backgroundColor = "blue";
+        });
+    });
 });
