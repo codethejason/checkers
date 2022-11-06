7865e9ad2fa8fab74c855a7c425aeddf4423d628
Tanner Dillon <tanner.dillon969@gmail.com>
2022-11-05 20:33:02 -0600

    Oops

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ window.onload = function () {
       player1: 0,
       player2: 0
     },
-    playerTurn: 1,
+    playerTurn: 2,
     jumpexist: false,
     continuousjump: false,
     tilesElement: $('div.tiles'),
