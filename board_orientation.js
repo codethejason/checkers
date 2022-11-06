a0f4da7cb05efde7dec4bd6b5d81ab472b43eaae
Android-JCPP-Servo <astewart1138@gmail.com>
2022-11-05 20:29:28 -0500

    Begun board orientation attempt

--- board_orientation.js
+++ board_orientation.js
@@ -1,3 +1,38 @@
 window.addEventListener('load', () => {
-    
+    console.log('page is fully loaded');
+
+    // INITIALIZE GAMEBOARD
+    let gameBoard = [];
+
+    // GET DIRECTIONAL OPTIONS
+    let normal = document.getElementById('default');
+    let reversed = document.getElementById('reversed');
+
+    // LISTEN FOR COLOR BUTTON CLICKS
+    // NORMAL
+    normal.addEventListener('click', () => {
+        gameBoard = [
+            [0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 2, 0, 2, 0, 2, 0],
+            [0, 2, 0, 2, 0, 2, 0, 2],
+            [2, 0, 2, 0, 2, 0, 2, 0]
+        ];
+    });
+    // REVERSED
+    reversed.addEventListener('click', () => {
+        gameBoard = [
+            [1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 2, 0, 2, 0, 2],
+            [2, 0, 2, 0, 2, 0, 2, 0],
+            [0, 2, 0, 2, 0, 2, 0, 2]
+        ];
+    });
 })
